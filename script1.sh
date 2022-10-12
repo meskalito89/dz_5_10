a3aa4617c3798614debb79506b73346b57702a4c
meskalito89 <rokinea89mone@mail.ru>
2022-10-13 00:02:43 +0500

    its alive!!!

--- script1.sh
+++ script1.sh
@@ -12,10 +12,16 @@ fi
 if [[ -f $2 ]]; then
     PASS_FILE=$2
 else 
-    echo 'ERROR: second argument shold be a file with user passwords'
+    echo 'ERROR: second argument should be a file with user passwords'
 fi
 
 
 if [[ -f $USER_FILE  && -f $PASS_FILE ]]; then
-    echo 'asdf'
+    sed -i 's/_//g' $PASS_FILE # there vas _ sighn in pass file. I remove it for matching with user name
+    for username in `cat $USER_FILE `; do
+        password=`cat $PASS_FILE | grep $username | cut -d: -f2`
+        useradd -m -p $password $username
+        echo "Greetings!\nHello $username, it nice to meet you!" > /home/$username/greeting.txt
+        sed -i '$ s/bin\/sh/bin\bash/' /etc/passwd
+    done
 fi
